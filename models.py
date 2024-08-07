cf70b4ae513f6dddf55805d40020eb1db35cd714
Tobias Oberrauch <tobias.oberrauch@audius.de>
2024-08-07 11:41:54 +0200

    first commit

---
+++ models.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
+    def __repr__(self):
+        return f'<Item {self.name}>'
